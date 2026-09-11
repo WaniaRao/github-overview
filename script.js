235fc71193bedd188a4327b25fe6ca7bad37ecf1
umaimarao <umaima.ra0000@gmail.com>
2026-09-11 04:50:07 -0700

    class2

---
+++ script.js
@@ -0,0 +1,50 @@
+// Q1
+var studentNames = ["wania"]
+
+console.log(studentNames)
+
+
+// Q2
+var studentNames = new Array("Zeehan","Ahil","Adan")
+
+console.log(studentNames)
+
+
+// Q3
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta"]
+
+console.log(cities)
+
+
+// Q4
+var numbers = [10, 20, 30, 40, 50]
+
+console.log(numbers)
+
+
+// Q5
+var answers = [true, false, true, false]
+
+console.log(answers)
+
+
+// Q6
+var mixedArray = ["Wini", 20, true, "Karachi", 5000]
+
+console.log(mixedArray)
+
+
+// Q7
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"]
+
+document.write("<h2>Educational Qualifications in Pakistan</h2>")
+document.write("<br>1. " + qualifications[0])
+document.write("<br>2. " + qualifications[1])
+document.write("<br>3. " + qualifications[2])
+document.write("<br>4. " + qualifications[3])
+document.write("<br>5. " + qualifications[4])
+document.write("<br>6. " + qualifications[5])
+document.write("<br>7. " + qualifications[6])
+document.write("<br>8. " + qualifications[7])
+
+console.log(qualifications)
